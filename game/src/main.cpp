393f5c0b2f8bc718f4afe5d77a5550fa329fce81
Lander Fannes <lander.fannes@gmail.com>
2015-06-01 20:52:15 +0200

    bal wordt getekend op het scherm

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,17 +1,37 @@
 #include "SFML/Window.hpp"
+#include "SFML/Graphics.hpp"
 #include <iostream>
 
 int main()
 {
     std::cout << "Hello world." << std::endl;
-    sf::Window window(sf::VideoMode(800, 600), "Lander zijn game");
+
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Lander zijn game");
+
+    //Bal aanmaken om te tekenen
+    sf::CircleShape bal;
+    bal.setRadius(40);
+    bal.setPosition(400-40, 300-40);
+    bal.setFillColor(sf::Color::Cyan);
+
     while (window.isOpen())
     {
+        //Kijken of we moeten afsluiten
         sf::Event event;
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
                 window.close();
+        }
+
+        //Teken het scherm
+        {
+            //Wis het scherm
+            window.clear();
+            //Teken de bal
+            window.draw(bal);
+            //Wissel de schermbuffers: dan pas zie je het resultaat
+            window.display();
         }
     }
     return 0;
