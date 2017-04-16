66af92c8ef954c63e7eb4901f6c73860a8761332
lfannes <lander.fannes@gmail.com>
2017-04-16 11:45:30 +0200

    SFML Tutorial added

---
+++ c++/SFML/Tutorial/main.cpp
@@ -0,0 +1,72 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <string>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(640, 640), "Lander Application");
+
+    sf::Font font;
+    if(!font.loadFromMemory("arial.ttf"))
+    {
+        std::cout << "Font not found" << std::endl;
+        return -1;
+    }
+
+
+    sf::Text text;
+    text.setFont(font);
+    text.setString("Kaka pipi");
+    text.setCharacterSize(24);
+    text.setFillColor(sf::Color::Red);
+    text.setPosition(315.f, 20.f);
+
+
+    sf::RectangleShape line(sf::Vector2f(150, 5));
+    line.setPosition(320.f, 320.f);
+
+    sf::CircleShape shape(80, 8);
+    shape.setFillColor(sf::Color::Cyan);
+    shape.setRadius(40.f);
+    shape.setPosition(100.f, 100.f);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+            {
+                shape.move(-3, 0);
+                line.rotate(-3);
+            }
+
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+            {
+                shape.move(3, 0);
+                line.rotate(3);
+            }
+
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+            {
+                shape.move(0, -3);
+            }
+
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+            {
+                shape.move(0, 3);
+            }
+
+            if(event.type == event.Closed)
+            {
+                window.close();
+            }       
+        }
+    window.clear();
+    window.draw(shape);
+    window.draw(line);
+    window.draw(text);
+    window.display();
+    }
+    
+}
--- c++/SFML/test/main.cpp
+++ c++/SFML/test/main.cpp
@@ -1,22 +1,43 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
+using namespace sf;
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(640, 480), "SFML Application");
-    sf::CircleShape shape;
-    shape.setRadius(40.f);
-    shape.setPosition(100.f, 100.f);
-    shape.setFillColor(sf::Color::Cyan);
+    sf::RenderWindow window(sf::VideoMode(640, 480), "Lander Application", sf::Style::Close | sf::Style::Resize);
+    sf::CircleShape shape(50);
+    shape.setFillColor(sf::Color(100, 250, 50));
     while (window.isOpen())
     {
     sf::Event event;
     while (window.pollEvent(event))
     {
-        if (event.type == sf::Event::Closed)
+
+        switch(event.type)
+        {
+            case sf::Event::Closed:
             window.close();
+            break;
+            case sf::Event::Resized:
+            printf("New window width:%i New window height:%i\n", event.size.width ,event.size.height);
+            break;
+
+        }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+        {
+
+        }
+
+        if(event.type == event.Closed)
+        {
+            window.close();
+        }       
+    }
+
     }
     window.clear();
     window.draw(shape);
     window.display();
-    }
+    return 0;
 }
